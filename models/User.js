1e51ec29dcd9d3ae550416e3bc39074ed2145ae7
Gray Lewis <grayjordann@gmail.com>
2021-01-29 13:00:22 +0100

    registration working

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema({
   username: {
@@ -15,4 +16,22 @@ const userSchema = new Schema({
     type: String,
     required: true,
   }
-})
+});
+
+userSchema.pre('save', function encryptPassword(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, (hashErr, hash) => {
+      if (hashErr) return next(hashErr);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 require('dotenv').config()
 
 const express = require('express');
+const { check } = require('express-validator')
 const session = require('express-session');
 const MongoDBSesssion = require('connect-mongodb-session')(session);
 const mongoose = require('mongoose');
-const UserModel = require('./models/User');
+const User = require('./models/User');
 
-const DB_KEY = process.env.MONGO_DB_KEY;
-
-const MongoURI = `mongodb+srv://-----:${process.env.MONGO_DB_KEY}@cluster-.-----.mongodb.net/workTimer?retryWrites=true&w=majority`
-//                                    ^Insert password here                                  ^Insert db name here
+const MongoURI = `mongodb://example:${process.env.MONGO_DB_KEY}@127.0.0.1/tutoring?retryWrites=true&w=majority`
+//                                  ^Insert password here       ^URL here ^Insert db name here
 
 mongoose.connect(MongoURI, {
   useNewUrlParser: true,
@@ -29,15 +28,39 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send("hello")
+  res.send("Welcome to my backend demonstration!")
 })
 
 app.get('/logout', (req, res) => {
 
+})
+
+app.post('/register', [
+  check('email', 'Email is invalid').isEmail().normalizeEmail(),
+  check('password', 'Password must be at least 6 characters and less than 20').isLength({ min: 6, max: 20 }),
+], async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    // Make sure this account doesn't already exist
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(401).json({ message: 'The email address you have entered is already associated with another account.' });
+
+    const newUser = new User({ ...req.body });
+    const user_ = await newUser.save();
+
+    res.send(user_)
+} catch (error) {
+    console.log(error)
+    res.status(500).json({success: false, message: error.message})
+}
 })
 
 app.listen(PORT, console.log(`Server running on ${PORT}`))
